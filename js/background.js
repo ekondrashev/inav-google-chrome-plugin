560c7e877dac83112062263728717f7ba690614d
grisov <grisov.gmail@mailnull.com>
2018-04-05 01:21:30 +0300

    Google API request method has added

--- js/background.js
+++ js/background.js
@@ -6,7 +6,7 @@ window.gapi.client = {};
 
 var access_token = undefined;
 
-// 
+// Authorization method in Google services
 gapi.auth.authorize = function (params, callback) {
     if (typeof callback !== 'function')
         throw new Error('callback required');
@@ -28,7 +28,65 @@ gapi.auth.authorize = function (params, callback) {
     chrome.identity.getAuthToken(details, callbackWrapper);
 };
 
+// Google API request method
+gapi.client.request = function (args) {
+    if (typeof args !== 'object')
+        throw new Error('args required');
+    if (typeof args.callback !== 'function')
+        throw new Error('callback required');
+    if (typeof args.path !== 'string')
+        throw new Error('path required');
 
+    if (args.root && args.root === 'string') {
+        var path = args.root + args.path;
+    } else {
+        var path = 'https://www.googleapis.com' + args.path;
+    }
+
+    if (typeof args.params === 'object') {
+        var deliminator = '?';
+        for (var i in args.params) {
+            path += deliminator + encodeURIComponent(i) + "="
+                + encodeURIComponent(args.params[i]);
+            deliminator = '&';
+        }
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open(args.method || 'GET', path);
+    xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+    if (typeof args.body !== 'undefined') {
+        xhr.setRequestHeader('content-type', 'application/json');
+        xhr.send(JSON.stringify(args.body));
+    } else {
+        xhr.send();
+    }
+    
+    xhr.onerror = function () {
+        // Todo and error handling.
+        debugger;
+    };
+
+    xhr.onload = function() {
+        var rawResponseObject = {
+            // Todo: body, headers.
+            gapiRequest: {
+                data: {
+                    status: this.status,
+                    statusText: this.statusText
+                }
+            }
+        };
+        
+        var rawResp = JSON.stringify(rawResponseObject);
+        if (this.response) {
+            var jsonResp = JSON.parse(this.response);
+            args.callback(jsonResp, rawResp);
+        } else {
+            args.callback(null, rawResp);
+        };
+    };
+}
 
 // Awaiting the request and giving the answer
 chrome.runtime.onMessage.addListener(function (msg, sender, send_response) {
